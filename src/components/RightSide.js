ebcccb66ed991a02e0efac46356edc56c9ddb68f
asmatar <deruelle.arthur@gmail.com>
2021-06-01 13:02:01 +0200

    right side done css

--- src/components/RightSide.js
+++ src/components/RightSide.js
@@ -31,8 +31,17 @@ const RightSide = () => {
                             </div>
                     </li>
                 </FeedList>
-
-            </FollowCard>        
+               < Recommendation>
+                    View all recommendations
+                    <img src="/images/right-icon.svg" alt="" />
+                </Recommendation>
+            </FollowCard>    
+            <BannerCard>
+                <img
+                src="https://static-exp1.licdn.com/scds/common/u/images/promo/ads/li_evergreen_jobs_ad_300x250_v1.jpg"
+                alt=""
+                />
+            </BannerCard>    
         </Container>
     )
 }
@@ -100,3 +109,15 @@ const Avatar = styled.div`
     height: 48px;
     margin-right: 8px;
 `
+const Recommendation = styled.a`
+  color: #0a66c2;
+  display: flex;
+  align-items: center;
+  font-size: 14px;
+`;
+const BannerCard = styled(FollowCard)`
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
