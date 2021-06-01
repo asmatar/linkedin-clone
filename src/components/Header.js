89e5873b96d4d134fb54ff25157667f7ef9138b7
asmatar <deruelle.arthur@gmail.com>
2021-06-01 10:22:50 +0200

    signout component

--- src/components/Header.js
+++ src/components/Header.js
@@ -55,6 +55,24 @@ function Header() {
                             </a>
                         </NavList>
 
+                        <User>
+                            <a>
+                                <img src="/images/user.svg" alt="" />
+                                <span>Me</span>
+                                <img src="/images/down-icon.svg" alt="" />
+                            </a>
+                            <SignOut>
+                                <a> sign out</a>
+                            </SignOut>
+                        </User>
+
+                        <Work>
+                            <a>
+                                <img src="/images/nav-work.svg" alt="" />
+                                <span>Work</span>
+                                <img src="/images/down-icon.svg" alt="" />
+                            </a>
+                        </Work>
                     </NavListWrap>
                 </Nav>
             </Content>
@@ -185,3 +203,42 @@ const NavList = styled.li`
         }
     }
 `
+const SignOut = styled.div`
+    position: absolute;
+    top: 45px;
+    background: white;
+    border-radius: 0 0 5px 5px;
+    width: 100px;
+    height: 40px;
+    font-size: 16px;
+    transition-duration: 167ms;
+    display: none;
+    text-align: center;
+   
+`
+const User = styled(NavList)`
+    a > svg {
+        width: 24px;
+        border-radius: 50%;
+    }
+    a > img {
+        width: 24px;
+        height: 24px;
+        border-radius: 50%;
+    }
+    span {
+        display: flex;
+        align-items: center;
+    }
+    &:hover {
+        ${SignOut} {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+    }
+`
+const Work = styled(User)`
+    border-left: 1px solid rgba(0, 0, 0, 0.08);
+`
+
