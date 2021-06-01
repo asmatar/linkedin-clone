76dbff534100c37b08285905384d2c83d244f57d
asmatar <deruelle.arthur@gmail.com>
2021-06-01 08:52:02 +0200

    login css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Login from './components/Login';
+
 
 function App() {
   return (
     <div className="App">
-        <h2>let's build linkedin clone</h2>
+        <Router>
+          <Switch>
+            <Route exact path='/'>
+              <Login />   
+            </Route>
+          </Switch>
+        </Router>
     </div>
   );
 }
