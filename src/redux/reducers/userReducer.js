c1a8820d45cd10f8967e31524fd46b81b1f63db4
asmatar <deruelle.arthur@gmail.com>
2021-06-01 19:51:03 +0200

    set up redux

---
+++ src/redux/reducers/userReducer.js
@@ -0,0 +1,12 @@
+const InitialState = {
+    user: null,
+};
+
+const userReducer = (state= InitialState, action) => {
+    switch (action.type){
+        default:
+            return state
+    }
+}
+
+export default userReducer;
