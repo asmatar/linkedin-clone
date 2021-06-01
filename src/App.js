41f6b29901376fb9d71796a8a246227b7ba367b0
asmatar <deruelle.arthur@gmail.com>
2021-06-01 09:15:26 +0200

    header search bar css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
 import Login from './components/Login';
 
 
 function App() {
+  
   return (
     <div className="App">
         <Router>
           <Switch>
             <Route exact path='/'>
               <Login />   
+            </Route>
+            <Route exact path='/home'>
+              <Header />
+              <Home />
             </Route>
           </Switch>
         </Router>
